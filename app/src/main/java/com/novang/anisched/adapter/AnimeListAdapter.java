3793eda888363ecb6108e2aa0acf19ef942f3fd2
Novang <qkdxorjs1002@naver.com>
2021-02-10 09:12:03 +0900

    Implement schedule list

---
+++ app/src/main/java/com/novang/anisched/adapter/AnimeListAdapter.java
@@ -0,0 +1,81 @@
+package com.novang.anisched.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.novang.anisched.R;
+import com.novang.anisched.model.anissia.Anime;
+
+import java.util.List;
+
+public class AnimeListAdapter extends RecyclerView.Adapter<AnimeListAdapter.ViewHolder> {
+
+    private List<Anime> animeList;
+    private OnItemClickListener onItemClickListener;
+
+    public AnimeListAdapter() {
+        onItemClickListener = null;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = (View) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.layout_anime_list_item, parent, false);
+
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Anime anime = animeList.get(position);
+
+        TextView title = holder.view.findViewById(R.id.title);
+
+        title.setText(anime.getSubject());
+
+        holder.view.setOnClickListener(v -> {
+            if (onItemClickListener != null) {
+                onItemClickListener.onItemClick(v, anime);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        if (animeList == null) {
+
+            return 0;
+        }
+
+        return animeList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public View view;
+
+        public ViewHolder(View v) {
+            super(v);
+            view = v;
+        }
+    }
+
+    public void updateList(List<Anime> list) {
+        animeList = list;
+        notifyDataSetChanged();
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(View v, Anime anime);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener i) {
+        this.onItemClickListener = i;
+    }
+
+}
--- app/src/main/java/com/novang/anisched/ui/main/MainFragment.java
+++ app/src/main/java/com/novang/anisched/ui/main/MainFragment.java
@@ -7,16 +7,22 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.novang.anisched.R;
+import com.novang.anisched.adapter.AnimeListAdapter;
 
 public class MainFragment extends Fragment {
 
-    private MainViewModel mViewModel;
+    private MainViewModel viewModel;
+
+    private RecyclerView animeListView;
+    private AnimeListAdapter animeListAdapter;
 
     public static MainFragment newInstance() {
         return new MainFragment();
@@ -32,8 +38,31 @@ public class MainFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
-        // TODO: Use the ViewModel
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+
+        initReferences();
+        initObservers();
+        initEvents();
+
+        viewModel.callSchedule(0);
+    }
+
+    public void initReferences() {
+        animeListView = getView().findViewById(R.id.anime_list_View);
+        animeListAdapter = new AnimeListAdapter();
+
+        animeListView.setLayoutManager(new LinearLayoutManager(getContext()));
+        animeListView.setAdapter(animeListAdapter);
+    }
+
+    public void initObservers() {
+        viewModel.animeList.observe(getViewLifecycleOwner(), animes -> {
+            animeListAdapter.updateList(animes);
+        });
+    }
+
+    public void initEvents() {
+
     }
 
 }
