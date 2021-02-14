7970816bd0218041177b59540797329d0f115612
Novang <qkdxorjs1002@naver.com>
2021-02-14 22:57:08 +0900

    Add contents to display more info

--- app/src/main/java/com/novang/anisched/ui/detail/DetailActivity.java
+++ app/src/main/java/com/novang/anisched/ui/detail/DetailActivity.java
@@ -1,31 +1,51 @@
 package com.novang.anisched.ui.detail;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+import com.google.android.material.appbar.AppBarLayout;
 import com.novang.anisched.R;
 import com.novang.anisched.adapter.CaptionListAdapter;
 import com.novang.anisched.adapter.GenreListAdapter;
+import com.novang.anisched.tool.GlideApp;
 
 public class DetailActivity extends AppCompatActivity {
 
     private DetailViewModel viewModel;
-    private int id;
 
+    private AppBarLayout appBar;
     private ImageView animeThumbnail;
     private TextView animeSubject;
+    private ImageView animeTmdbPoster;
+    private TextView animeTime;
     private TextView animeStartDate;
     private TextView animeEndDate;
-    private Button animeWebsite;
+    private ImageView animeStatusLive;
+    private ImageView animeStatusOff;
+    private TextView tmdbTitle;
+    private TextView tmdbOverview;
+    private ProgressBar tmdbRating;
+    private TextView tmdbRatingCount;
+    private TextView tmdbRatingDecimal;
+    private ConstraintLayout websiteHeader;
 
     private RecyclerView genreListView;
     private GenreListAdapter genreListViewAdapter;
@@ -38,21 +58,30 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         viewModel = new ViewModelProvider(this).get(DetailViewModel.class);
-        id = getIntent().getIntExtra("id", -1);
 
         initReferences();
         initObservers();
         initEvents();
 
-        viewModel.callAnimeInfo(id);
+        viewModel.callAnimeInfo(getIntent().getIntExtra("id", -1));
     }
 
     public void initReferences() {
+        appBar = findViewById(R.id.appBar);
         animeThumbnail = findViewById(R.id.anime_info_thumbnail);
         animeSubject = findViewById(R.id.anime_info_subject);
+        animeTmdbPoster = findViewById(R.id.anime_info_tmdb_poster);
+        animeTime = findViewById(R.id.anime_info_time);
         animeStartDate = findViewById(R.id.anime_info_startdate);
         animeEndDate = findViewById(R.id.anime_info_enddate);
-        animeWebsite = findViewById(R.id.anime_info_website);
+        animeStatusLive = findViewById(R.id.anime_info_status_live);
+        animeStatusOff = findViewById(R.id.anime_info_status_off);
+        tmdbTitle = findViewById(R.id.tmdb_title);
+        tmdbOverview = findViewById(R.id.tmdb_overview);
+        tmdbRating = findViewById(R.id.tmdb_rating);
+        tmdbRatingCount = findViewById(R.id.tmdb_rating_count);
+        tmdbRatingDecimal = findViewById(R.id.tmdb_rating_decimal);
+        websiteHeader = findViewById(R.id.website_header);
         genreListView = findViewById(R.id.anime_info_genre_list_view);
         genreListViewAdapter = new GenreListAdapter();
         captionListView = findViewById(R.id.caption_list_View);
@@ -62,18 +91,61 @@ public class DetailActivity extends AppCompatActivity {
         genreListView.setAdapter(genreListViewAdapter);
         captionListView.setLayoutManager(new LinearLayoutManager(this));
         captionListView.setAdapter(captionListAdapter);
+
+        appBar.setExpanded(false, false);
+        animeStatusLive.setVisibility(View.GONE);
+        animeStatusOff.setVisibility(View.GONE);
     }
 
     public void initObservers() {
         viewModel.animeInfo.observe(this, anime -> {
+            viewModel.searchTMDB(getString(R.string.tmdb_api_key), anime.getSubject());
             animeSubject.setText(anime.getSubject());
+            animeTime.setText(anime.getTime());
             animeStartDate.setText(anime.getStartDate());
             animeEndDate.setText(anime.getEndDate());
-            animeWebsite.setOnClickListener(v -> {
+            if(anime.isStatus()) {
+                animeStatusLive.setVisibility(View.VISIBLE);
+            } else {
+                animeStatusOff.setVisibility(View.VISIBLE);
+            }
+            tmdbTitle.setText(anime.getSubject());
+            websiteHeader.setOnClickListener(v -> {
                 startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(anime.getWebsite())));
             });
             genreListViewAdapter.updateList(anime.getGenres());
             captionListAdapter.updateList(anime.getCaptionList());
+        });
+
+        viewModel.tmdbResult.observe(this, result -> {
+            tmdbTitle.setText(tmdbTitle.getText().toString().concat("\n").concat(result.getOriginalName()));
+            tmdbOverview.setText(result.getOverview());
+            tmdbRating.setProgress(result.getVoteDecimal());
+            tmdbRatingCount.setText(String.valueOf(result.getVoteCount()));
+            tmdbRatingDecimal.setText(String.valueOf(result.getVoteDecimal()));
+
+            GlideApp.with(this)
+                    .load(result.getBackdropPath("original"))
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .listener(new RequestListener<Drawable>() {
+                        @Override
+                        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                            appBar.setExpanded(true, true);
+                            return false;
+                        }
+                    })
+                    .into(animeThumbnail);
+
+            GlideApp.with(this)
+                    .load(result.getPosterPath("w400"))
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .override(Target.SIZE_ORIGINAL)
+                    .into(animeTmdbPoster);
         });
     }
 
--- app/src/main/java/com/novang/anisched/ui/detail/DetailViewModel.java
+++ app/src/main/java/com/novang/anisched/ui/detail/DetailViewModel.java
@@ -5,21 +5,28 @@ import androidx.lifecycle.ViewModel;
 
 import com.novang.anisched.model.anissia.Anime;
 import com.novang.anisched.model.anissia.Caption;
+import com.novang.anisched.model.tmdb.Result;
 import com.novang.anisched.repository.anissia.AnissiaRepository;
+import com.novang.anisched.repository.tmdb.TMDBRepository;
 
 import java.util.List;
 
 public class DetailViewModel extends ViewModel {
 
     private AnissiaRepository anissiaRepository;
+    private TMDBRepository tmdbRepository;
 
     public MutableLiveData<Anime> animeInfo;
     public MutableLiveData<List<Caption>> captionList;
+    public MutableLiveData<Result> tmdbResult;
 
     public DetailViewModel() {
         anissiaRepository = new AnissiaRepository();
+        tmdbRepository = new TMDBRepository();
+
         animeInfo = new MutableLiveData<>();
         captionList = new MutableLiveData<>();
+        tmdbResult = new MutableLiveData<>();
     }
 
     public void callAnimeInfo(int id) {
@@ -34,4 +41,18 @@ public class DetailViewModel extends ViewModel {
         });
     }
 
+    public void searchTMDB(String apiKey, String keyword) {
+        String match = "[^\uAC00-\uD7A3xfe0-9a-zA-Z\\s]";
+        String keywordWoSp = keyword.replaceAll(match, " ");
+
+        tmdbRepository.search(apiKey, "ko-KR", keywordWoSp).observeForever(searches -> {
+            List<Result> result = searches.getResultList();
+
+            if(result != null) {
+                if(result.size() > 0) {
+                    tmdbResult.postValue(searches.getResultList().get(0));
+                }
+            }
+        });
+    }
 }
