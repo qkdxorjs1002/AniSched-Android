41ce210cdc052395d871ce66970d22f42ae4e660
Novang <qkdxorjs1002@naver.com>
2021-02-14 22:48:58 +0900

    Glide

---
+++ app/src/main/java/com/novang/anisched/tool/GlideTool.java
@@ -0,0 +1,9 @@
+package com.novang.anisched.tool;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class GlideTool extends AppGlideModule {
+
+}
