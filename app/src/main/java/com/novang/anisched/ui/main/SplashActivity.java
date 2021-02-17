29158ea73376cc314c2aea912882b8327735ac98
Novang <qkdxorjs1002@naver.com>
2021-02-18 05:03:58 +0900

    Splash

---
+++ app/src/main/java/com/novang/anisched/ui/main/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.novang.anisched.ui.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
